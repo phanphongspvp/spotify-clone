b1ea42c0ac033d8f2aaa8bd90eeb895fe1d96bb1
phongpv2 <phongpv2@bap.jp>
2024-04-22 16:16:47 +0700

    push

---
+++ src/components/CategorySelect.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, toRefs } from 'vue'
+import { RouterLink } from 'vue-router'
+import uniqolor from 'uniqolor'
+const randColor = ref('')
+
+randColor.value = uniqolor.random()
+
+const props = defineProps({ category: String, image: String })
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import CategorySelect from '../components/CategorySelect.vue'
+</script>
+
+<template>
+    <div class="p-8">
+        <button 
+          type="button"
+          class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+          Recommended songs
+        </button>
+    
+        <div class="py-1.5"></div>
+    
+        <div class="flex items-center">
+          <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300" />
+        </div>
+      </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
